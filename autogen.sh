9280a98ceacbb798c4515a721ffab3dc8c3a7e94
Mohamad mehdi Kharatizadeh <m_kharatizadeh@yahoo.com>
2018-07-12 15:25:00 +0430

    automatic generation of GNU autotools companion files from git

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
 
-autoreconf --verbose --install
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+cd $DIR && git shortlog -e -s -n > AUTHORS
+cd $DIR && git tag -l -n1000 > NEWS
+cd $DIR && cp README.md README
+cd $DIR && cp LICENSE COPYING
+
+echo "" > $DIR/ChangeLog
+for hash in `cd $DIR && git log --pretty=format:"%H"`; do
+    echo `cd $DIR && git log -1 --date=short --pretty=format:"%ad%x09%an%x09<%ae>" $hash` >> $DIR/ChangeLog
+    echo "" >> $DIR/ChangeLog
+    for file in `cd $DIR && git show --pretty="" --name-only $hash`; do
+        echo -e '\t' $file: `cd $DIR && git log -1 --pretty=format:"%s" $hash` >> $DIR/ChangeLog
+    done
+    echo "" >> $DIR/ChangeLog
+done
+
+autoreconf --verbose --install
