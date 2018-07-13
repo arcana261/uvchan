08a71babe3457f35bb059955e9905e2d3c0854af
Mohamad mehdi Kharatizadeh <m_kharatizadeh@yahoo.com>
2018-07-13 06:56:41 +0430

    some improvement to build system

---
+++ src/uvchan/uvchan.h
@@ -0,0 +1,10 @@
+#ifndef UVCHAN_UVCHAN_H__
+#define UVCHAN_UVCHAN_H__
+
+/**
+ * @mainpage
+ * libuvchan is a library built upon infamous libuv which is
+ * aimed to provide API akin to channels in GoLang
+ */
+
+#endif  // UVCHAN_UVCHAN_H__
