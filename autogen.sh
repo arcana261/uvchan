34d8c3eaf1bfe176a380a297b120a0d337a0cb00
Mohamad mehdi Kharatizadeh <m_kharatizadeh@yahoo.com>
2018-07-13 23:10:44 +0430

    updated travis ci build

--- autogen.sh
+++ autogen.sh
@@ -9,7 +9,7 @@ cd $DIR && git tag -l -n1000 > NEWS
 cd $DIR && cp README.md README
 cd $DIR && cp LICENSE COPYING
 
-if [ x$1 != "--no-changelog"]; then
+if [ x$1 != "--no-changelog" ]; then
     echo "" > $DIR/ChangeLog
     for hash in `cd $DIR && git log --pretty=format:"%H"`; do
         echo `cd $DIR && git log -1 --date=short --pretty=format:"%ad%x09%an%x09<%ae>" $hash` >> $DIR/ChangeLog
