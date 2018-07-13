bf8787a28e1112a4870ca72a56904fc2791e2222
Mohamad mehdi Kharatizadeh <m_kharatizadeh@yahoo.com>
2018-07-13 06:08:24 +0430

    made tests TAP compliant

--- test/testing.h
+++ test/testing.h
@@ -9,13 +9,14 @@
 #include <sys/time.h>
 #include "./config.h"
 
-char MSG[2048];
+char MSG[4096];
 jmp_buf JUMP_BUF;
 int ARGC;
 char** ARGV;
 typedef void (*TEST_FN)(void);
 TEST_FN TESTS[1000];
 const char* TEST_NAMES[1000];
+int TEST_MASK[1000];
 int TESTS_COUNT = 0;
 int EXIT_CODE = 0;
 int PLAN_START_INDEX = 1;
@@ -33,29 +34,102 @@ int _t_fail(const char* file, int line, const char* msg, ...) {
   return 0;
 }
 
+void _t_help() {
+  printf("%s v%s\n", PACKAGE_NAME, PACKAGE_VERSION);
+  printf("\t-h, --help: show this help message\n");
+  printf("\t-l, --list: show list of test cases available\n");
+  printf("\t-c, --count: print number of tests in test suit and exit\n");
+  printf("\t-t, --test: run specific test\n");
+  printf("\t-e, --exclude: skip running specific test\n");
+  printf("\t-i, --index: starting test index to use for TAP(Test Anything Protocol), Default: 1\n");
+  printf("\nsend bug reports to %s\n\n", PACKAGE_BUGREPORT);
+}
+
 void _t_init(int argc, char** argv) {
   int i;
+  int j;
   ARGC = argc;
   ARGV = argv;
+  int specific_countered = 0;
+  int found;
+
+  for (i = 0; i < TESTS_COUNT; i++) {
+    TEST_MASK[i] = 1;
+  }
 
   for (i = 1; i < argc; i++) {
     if (!strcmp(argv[i], "-h") || !strcmp(argv[i], "--help")) {
-      printf("%s v%s\n", PACKAGE_NAME, PACKAGE_VERSION);
-      printf("\t-h, --help: show this help message\n");
-      printf("\t-l, --list: show list of test cases available\n");
-      printf("\t-c, --count: print number of tests in test suit and exit\n");
-      printf("\t-t, --test: run specific test\n");
-      printf("\t-e, --exclude: skip running specific test\n");
-      printf("\t-i, --index: starting test index to use for TAP(Test Anything Protocol), Default: 1\n");
-      printf("\nsend bug reports to %s\n\n", PACKAGE_BUGREPORT);
+      _t_help();
       exit(0);
     } else if (!strcmp(argv[i], "-c") || !strcmp(argv[i], "--count")) {
       printf("%d\n", TESTS_COUNT);
       exit(0);
+    } else if (!strcmp(argv[i], "-i") || !strcmp(argv[i], "--index")) {
+      if ((i + 1) >= argc || !sscanf(argv[i+1], "%d", &PLAN_START_INDEX)) {
+        _t_help();
+        exit(-1);
+      }
+
+      i = i + 1;
     } else if (!strcmp(argv[i], "-l") || !strcmp(argv[i], "--list")) {
-      printf("%s v%s\n", PACKAGE_NAME, PACKAGE_VERSION);
-      printf("\nsend bug reports to %s\n\n", PACKAGE_BUGREPORT);
-    }
+      for (j = 0; j < TESTS_COUNT; j++) {
+        printf("%s\n", TEST_NAMES[j]);
+      }
+      exit(0);
+    } else if (!strcmp(argv[i], "-t") || !strcmp(argv[i], "--test")) {
+      if (!specific_countered) {
+        for (j = 0; j < TESTS_COUNT; j++) {
+          TEST_MASK[j] = 0;
+        }
+
+        specific_countered = 1;
+      }
+
+      if ((i + 1) >= argc) {
+        _t_help();
+        exit(-1);
+      }
+
+      found = 0;
+      for (j = 0; j < TESTS_COUNT; j++) {
+        if (!strcmp(TEST_NAMES[j], argv[i + 1])) {
+          found = 1;
+          break;
+        }
+      }
+
+      if (!found) {
+        printf("no test matching name: %s\n", argv[i + 1]);
+        exit(-1);
+      }
+
+      TEST_MASK[j] = 1;
+      i = i + 1;
+    } else if (!strcmp(argv[i], "-e") || !strcmp(argv[i], "--exclude")) {
+      if ((i + 1) >= argc) {
+        _t_help();
+        exit(-1);
+      }
+
+      found = 0;
+      for (j = 0; j < TESTS_COUNT; j++) {
+        if (!strcmp(TEST_NAMES[j], argv[i + 1])) {
+          found = 1;
+          break;
+        }
+      }
+
+      if (!found) {
+        printf("no test matching name: %s\n", argv[i + 1]);
+        exit(-1);
+      }
+
+      TEST_MASK[j] = 0;
+      i = i + 1;
+    } else {
+      _t_help();
+      exit(-1);
+    } 
   }
 }
 
@@ -74,35 +148,13 @@ void _format_dtime(struct timeval* start, struct timeval* stop, char* buffer,
   snprintf(buffer, n, "%d ms", delta);
 }
 
-void _t_run_test(void (*fn)(void), const char* name) {
-  int i;
-  int specific_enabled;
-  int specific_found;
+void _t_run_test(void (*fn)(void), const char* name, int index) {
   struct timeval stop, start;
   char dtime[100];
+  char* token;
 
-  specific_enabled = 0;
-  specific_found = 0;
-
-  for (i = 1; i < ARGC; i++) {
-    if (!strcmp(ARGV[i], "-h") || !strcmp(ARGV[i], "--help")) {
-      return;
-    } else if (!strcmp(ARGV[i], "-l") || !strcmp(ARGV[i], "--list")) {
-      printf("\t%s\n", name);
-      return;
-    } else if (!strcmp(ARGV[i], "-t") || !strcmp(ARGV[i], "--test")) {
-      specific_enabled = 1;
-    } else if (specific_enabled && !strcmp(ARGV[i], name)) {
-      specific_found = 1;
-    }
-  }
-
-  if (specific_enabled && !specific_found) {
-    return;
-  }
-
-  printf("%s... ", name);
-  fflush(stdout);
+  //printf("%s... ", name);
+  //fflush(stdout);
 
   gettimeofday(&start, NULL);
 
@@ -110,7 +162,14 @@ void _t_run_test(void (*fn)(void), const char* name) {
     gettimeofday(&stop, NULL);
     _format_dtime(&start, &stop, dtime, sizeof(dtime));
 
-    printf("ERR (%s)\n%s\n", dtime, MSG);
+    printf("not ok %d - %s # ASSERTION FAILED (took: %s)\n", index, name, dtime);
+    token = strtok(MSG, "\n");
+    while (token) {
+      printf("# %s\n", token);
+      token = strtok(NULL, "\n");
+    }
+    fflush(stdout);
+
     EXIT_CODE = -1;
   } else {
     fn();
@@ -118,7 +177,7 @@ void _t_run_test(void (*fn)(void), const char* name) {
     gettimeofday(&stop, NULL);
     _format_dtime(&start, &stop, dtime, sizeof(dtime));
 
-    printf("OK (%s)\n", dtime);
+    printf("ok %d - %s # (took: %s)\n", index, name, dtime);
     fflush(stdout);
   }
 }
@@ -131,14 +190,28 @@ int _t_run(int argc, char** argv) {
   _t_init(argc, argv);
   gettimeofday(&start, NULL);
 
+  printf("\n");
+  printf("##\n");
+  printf("## %s\n", argv[0]);
+  printf("##\n");
+  printf("\n");
+  fflush(stdout);
+
   for (i = 0; i < TESTS_COUNT; i++) {
-    _t_run_test(TESTS[i], TEST_NAMES[i]);
+    if (TEST_MASK[i]) {
+      _t_run_test(TESTS[i], TEST_NAMES[i], PLAN_START_INDEX + i);
+    } else {
+      printf("ok %d - %s # SKIP\n", PLAN_START_INDEX + i, TEST_NAMES[i]);
+      fflush(stdout);
+    }
   }
 
   gettimeofday(&stop, NULL);
   _format_dtime(&start, &stop, dtime, sizeof(dtime));
 
-  printf("\nTests Took: (%s)\n", dtime);
+  printf("\n");
+  printf("## took (%s)\n", dtime);
+  fflush(stdout);
 
   return EXIT_CODE;
 }
